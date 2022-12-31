56b366f88123112b2357246d344df46b1eeb8e3e
issoe <twmnno@gmail.com>
2022-12-31 12:16:10 +0700

    first commit

---
+++ springboot-backend/src/test/java/com/app/bookingroom/BookingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bookingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
