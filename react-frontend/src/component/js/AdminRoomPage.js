bdf2fee9c2d0f4aacdd2081d6be262a75732c238
issoe <twmnno@gmail.com>
2023-01-05 17:57:21 +0700

    optimize code

--- react-frontend/src/component/js/AdminRoomPage.js
+++ react-frontend/src/component/js/AdminRoomPage.js
@@ -1,14 +1,43 @@
-// import RoomCards from '../Cards/RoomCards';
 import React, { useEffect, useState } from 'react';
-import '../css/AdminRoomPage.css';
-
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios"
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+import '../css/AdminRoomPage.css';
 
 export default function AdminRoomPage() {
     const navigate = useNavigate();
     const [rooms, setRoom] = useState([]);
     const { hotelID } = useParams();
+
+    const [roomNumber, setRoomNumber] = useState('');
+    const [theStatus, setStatus] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = () => {
+        if (roomNumber === '' || theStatus === '' || price === '' || description === '')
+            console.log("4 fields must be filled")
+        else {
+            const adminAccount = {
+                "roomNumber": roomNumber,
+                "status": theStatus,
+                "price": price,
+                "description": description,
+                "hotelId": hotelID
+            }
+
+            axios.post('http://localhost:8082/room', adminAccount)
+                .then(res => {
+                    if (res.status === 200) {
+                        console.log("Add new room successfully by admin")
+                    }
+                })
+                .catch((error) => console.log(error))
+        }
+    }
 
     useEffect(() => {
         axios.get('http://localhost:8082/rooms', { params: { "id": hotelID } })
@@ -20,7 +49,44 @@ export default function AdminRoomPage() {
 
     return (
         <div className='background' >
-            <h2 className='text-center display-2'>Rooms:</h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" style={{ marginBottom: '16px' }}>
+                <Container>
+                    <Navbar.Brand href="#">Room-Admin page</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#">New room</Nav.Link>
+                            <Nav.Link href="#">Remove room</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/" >Log out</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <div className='newRoom'>
+                <form className='mb-3'>
+                    <input className="form-control" type="text" id="roomNumber" value={roomNumber}
+                        onChange={(e) => { setRoomNumber(e.target.value) }}
+                        placeholder='Room number' required />
+
+                    <input className="form-control" type="text" id="phone" value={theStatus}
+                        onChange={(e) => { setStatus(e.target.value) }}
+                        placeholder='Status' required />
+
+                    <input className="form-control" type="text" id="email" value={price}
+                        onChange={(e) => { setPrice(e.target.value) }}
+                        placeholder='Price' required />
+
+                    <input className="form-control" type="text" id="address" value={description}
+                        onChange={(e) => { setDescription(e.target.value) }}
+                        placeholder='Description' required />
+
+                    <button className='btn btn-primary' onClick={handleSubmit}>New Room</button>
+                </form>
+            </div>
+
             <div className="RoomCards">
                 {rooms.map((room, index) => {
                     return (
@@ -28,9 +94,11 @@ export default function AdminRoomPage() {
                             <div
                                 className="CompactRoomCard"
                                 layoutId="expandableCard"
-                                onClick={() => navigate('/CusCreateBooking')}>
+                                // onClick={() => navigate('/CusCreateBooking')}
+                                onClick={console.log("navigate to next page")}
+                            >
                                 <div className="infor">
-                                    <img src="../img/img.jpg" className='rounded' />
+                                    <img src="http://bob.codegym.vn/assets/images/Codegym-bob-RIGHTxWH200.png" className='rounded'></img>
                                     <div >
                                         <h5>Room number: {room.roomNumber}</h5>
                                         <span>Status: {room.status}</span>
